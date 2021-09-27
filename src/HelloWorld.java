2f631e30f920544f61531372dd01642d36f0353a
naomi <naom0649@stud.kea.dk>
2021-09-27 15:25:40 +0200

    first

---
+++ src/HelloWorld.java
@@ -0,0 +1,16 @@
+public class HelloWorld {
+    public static void main(String[] args) {
+
+        int age;
+        double heigth;
+        int shoeSize;
+
+        age = 22;
+        heigth = 1.80;
+        shoeSize = 39;
+
+        System.out.println(" age  heigth  shoeSize");
+
+    }
+
+}
